eda0f96c6844fa181282bad10970fef0468987e0
Tim Doren <dorentim@gmail.com>
2023-04-25 16:26:30 -0400

    finishing changes for the day. Just made small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,17 @@ import { AboutComponent } from './core/about/about.component';
 import { E404Component } from './core/e404/e404.component';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { User } from './user/user.class';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 const routes: Routes = [
 
-  { path: "", redirectTo: "user/list", component: UserListComponent },
+  { path: "", redirectTo: "user/list", pathMatch: "full" },
 
   { path: "/home", component: HomeComponent },
   { path: "/about", component: AboutComponent },
   { path: "**", component: E404Component},
   { path: "user/list", component: UserListComponent},
-  {path: "user/login", }
+  {path: "user/login", component: UserLoginComponent }
 
 ];
 
