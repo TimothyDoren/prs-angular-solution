ccde7363bf9f199b6b86a93dd4f5c1ceb539e506
Tim Doren <dorentim@gmail.com>
2023-04-26 11:23:17 -0400

    added user-change

--- src/app/user/user-change/user-change.component.ts
+++ src/app/user/user-change/user-change.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-change',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-change.component.css']
 })
 export class UserChangeComponent {
+
+  user!: User;
+  pageTitle = "User Change";
+
+  constructor(
+    private userSvc: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){}
+
+  save(){
+    this.userSvc.change(this.user).subscribe({
+      next: (res) => {
+        console.debug("User Changed!");
+        this.router.navigateByUrl("/user/list");
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    let id = this.route.snapshot.params["id"];
+    this.userSvc.get(id).subscribe({
+      next: (res) => {
+        console.debug("User:", res);
+        this.user = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
 
 }
