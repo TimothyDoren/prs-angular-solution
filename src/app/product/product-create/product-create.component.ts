65d5430e137adfbae555541ecd10e8cb2370da14
Tim Doren <dorentim@gmail.com>
2023-05-01 09:18:31 -0400

    after doing requestLine create over the weekend, I fixed up my product create to include an instance of the vendor

--- src/app/product/product-create/product-create.component.ts
+++ src/app/product/product-create/product-create.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Product } from '../product.class';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+import { VendorService } from 'src/app/vendor/vendor.service';
+import { Vendor } from '../../vendor/vendor.class'
 
 @Component({
   selector: 'app-product-create',
@@ -12,10 +14,12 @@ export class ProductCreateComponent {
 
   pageTitle = "Product Create";
   product: Product = new Product();
+  vendors: Vendor[] = []
 
   constructor(
     private prodSvc: ProductService,
-    private router: Router
+    private router: Router,
+    private venSvc: VendorService
   ){}
 
   save(): void {
@@ -31,6 +35,14 @@ export class ProductCreateComponent {
   }
 
   ngOnInit(): void {
-    
+    this.venSvc.list().subscribe({
+      next: (res) => {
+        console.debug("Vendors:", res);
+        this.vendors = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
   }
 }
